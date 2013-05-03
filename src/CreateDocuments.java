0059aa25ffd2d75454a9191a05bc25694fdacd4e
David Graham <david.graham.2@ucdconnect.ie>
2013-05-03 17:14:37 +0100

    updated create documents

--- src/CreateDocuments.java
+++ src/CreateDocuments.java
@@ -1,13 +1,19 @@
+package Assignment_1;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 import java.util.Vector;
 
 public class CreateDocuments {
 	private String file ="";
+	private Vector<String> vector = new Vector();
 	public CreateDocuments(String file){
 		this.file = file;
+
 	}
 	
 	private Vector<WebNewsDocument> create(){
@@ -19,9 +25,10 @@ public class CreateDocuments {
 			BufferedReader buffer = new BufferedReader(new  FileReader(file));
 			String line = "";
 			while((line = buffer.readLine())!= null){
-				String[] temp = line.split(" ");
+				String[] temp = line.split(" ", 2);
 				documents.add(doc = new WebNewsDocument(temp[0]));
-				System.out.println("i am a doc hear me roar!! "+ "\n" + doc.getBodyText() );
+				vector.add(temp[1]);
+			//	System.out.println("i am a doc hear me roar!! "+ "\n\n"+ temp[1] + "\n\n" + doc.getBodyText() );
 
 			}
 
@@ -32,8 +39,24 @@ public class CreateDocuments {
 		return documents;
 	}
 	
-	public Vector<WebNewsDocument> getDocuments(){
+	private void Makefiles() throws IOException{
+		Vector<WebNewsDocument> docs = this.create();
+		int i = 0;
+		
+		for(WebNewsDocument temp: docs){
+			System.out.println("im here "+vector.get(i));
+		FileWriter fw = new FileWriter("src\\"+vector.get(i)+".txt", false);
+		fw.write(temp.getBodyText());
+		fw.close();
+		i++;
+		}
+		
+	}
+	
+	public Vector<WebNewsDocument> getDocuments() throws IOException{
+		this.Makefiles();
 		return this.create();
+		
 	}
 
 }
