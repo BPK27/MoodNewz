ca086a98d46b7d17e70c0362f84140672f015a49
Blayne Keogh <blayne.keogh@ucdconnect.ie>
2013-05-06 19:43:53 +0100

    blah

--- src/Searcher.java
+++ src/Searcher.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Vector;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -22,7 +23,8 @@ public class Searcher {
     IndexSearcher searcher;
     TopScoreDocCollector collector;// = TopScoreDocCollector.create(hitsPerPage, true);
     StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_42);
-
+    public Vector<String> resultsHeadlines = new Vector<String>();
+    public Vector<String> resultsArticles = new Vector<String>();
     
 	public Searcher(String indexPath){
 		try{
@@ -52,13 +54,16 @@ public class Searcher {
 	
 	//print results
 	public void printHits(ScoreDoc[] hits){
+		//Vector<String> results = new Vector<String>();
 		try{
 			System.out.println("Found " + hits.length + " hits.");
 			for(int i=0;i<hits.length;++i) {
 			    int docId = hits[i].doc;
 			    float score = hits[i].score;			 
-			    Document d = searcher.doc(docId);			    
-			    System.out.println((i + 1) + ". " + d.get("headline") + "\t" + d.get("article")+" --- "+score);
+			    Document d = searcher.doc(docId);	
+			    resultsHeadlines.add(d.get("headline"));
+			    resultsArticles.add(d.get("article"));
+			    //System.out.println((i + 1) + ". " + d.get("headline") + "\t" + d.get("article")+" --- "+score);
 			}	
 		}catch(Exception e){e.printStackTrace();}
 	}
